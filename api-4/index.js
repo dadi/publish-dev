fcac1d5d9bb8c0a80d2a8b4343d0271317380330
Eduardo Bouças <mail@eduardoboucas.com>
2019-04-16 09:40:35 +0100

    refactor: rename api directory

--- api-4/index.js
+++
@@ -1,5 +0,0 @@
-var app = require('@dadi/api')
-
-app.start(function() {
-  console.log('Started')
-})
--- api-4/workspace/hooks/slugify.js
+++
@@ -1,8 +0,0 @@
-const slugify = require('slugify')
-
-module.exports = function (obj, type, data) {
-  if (obj[data.options.from]) {
-    obj[data.options.to] = slugify(obj[data.options.from], { lower: true })
-  }
-  return obj
-}
